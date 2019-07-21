6968b820b8c6dc5bc601b28dd01812bf4f8cba2f
Oscar Shoudt <shoudto@gmail.com>
2019-07-21 01:33:21 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+ # binding.pry
+  if array.length == 1 
+      array.first 
+      
+  elsif array.length == 2 
+      array.join(" and ")
+      
+  elsif array.length >= 3 
+    array[-1] = "and #{array[-1]}" 
+    array.join(", ")
+  end 
 end
